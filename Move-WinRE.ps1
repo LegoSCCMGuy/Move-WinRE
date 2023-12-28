1082507c6f3c7c103c14b0cb1249820bf8b544f9
Barry Harriman <31367660+LegoSCCMGuy@users.noreply.github.com>
2023-12-29 00:53:47 +1100

    Update Move-WinRE.ps1
    adding comments and outputs

--- Move-WinRE.ps1
+++ Move-WinRE.ps1
@@ -10,92 +10,100 @@
 #restore Partiion
 #HidePartition
 
+Write-host "Starting Process" -ForegroundColor White
+Write-host "-Gathering Disk Configs" -NoNewline -ForegroundColor White
+
 $disk = Get-Disk | where-object {$_.IsBoot -eq $true}
 
 if($disk -eq $null)
 {
-    Write-Host "Error Selecting Disks"
+    Write-Host "*** Error Selecting Disks ***" -ForegroundColor Red
     Stop-Transcript
     exit
 }
 else
 {
-    Write-Host " Found Boot Disk. Checking Partitions"
+    Write-Host " +Found Boot Disk" -ForegroundColor Green
 }
+
 $currentSize = $disk.size / 1024 /1024 /1024
 $allocatedSize = $disk.AllocatedSize /1024 /1024 /1024
 write-host " Current Disk Size: $($currentSize)GB"
 write-host " Current Allocated Size: $($allocatedSize)GB"
 
-if($currentSize - $allocatedSize -le 0.5)
+if($currentSize - $allocatedSize -le 1)
 {
-    write-host " Not enough Free Space on Disk. Expecting 500MB Exiting.."
+    write-host " Not enough Free Space on Disk. Expecting over 1GB Free Exiting.." -ForegroundColor Red
     Stop-Transcript
     exit
 }
 else
 {
-    write-host " Checking Partitions Order"
+    write-host " - Checking Partitions Order" -ForegroundColor White
 
 
     $partitions = Get-Partition -disk $disk
 
-    write-host "Number of Partiions Found: $($partitions.count)"
-    $partitions
-
-    write-host ""
-    write-host " Finding Recovery Partitions"
+    write-host "   Number of Partiions Found: $($partitions.count)"
+    write-host " - Finding Recovery Partitions - " -NoNewline -ForegroundColor Yellow
     $winrePartition = $partitions | where-object {$_.GptType -eq '{de94bba4-06d1-4d40-a16a-bfd50179d6ac}'}
     if($winrePartition -eq $null)
     {
-        Write-host "No WinRE Partitions Found. Exiting... "
+        Write-host "No WinRE Partitions Found. Exiting... " -ForegroundColor Red
         Stop-Transcript
         exit
     }
     else
     {
-        Write-host "WinRE Partition Found! Evaluating...."
+        Write-host "WinRE Partition Found!" -ForegroundColor Green
+        Write-Host " - Evaluating WinRE Partition - " -nonewline -ForegroundColor Yellow
         if ($partitions.count -ne $winrePartition.PartitionNumber)
         {
-            write-host "Not Last Partition. Aborting"
+            write-host "Not Last Partition. Aborting" -ForegroundColor Red
             Stop-Transcript
             exit
 
         }
         else
         {
-            write-host "Confirmed Partion is last and free space"
-            #check C:\windows\temp\winre
+            write-host "Confirmed Partion is last current partition" -ForegroundColor Green
+            write-host " - Checking for WinRE TempPath - " -NoNewline -ForegroundColor Yellow
             If(test-path -Path "C:\windows\temp\winre")
             {
-                write-host " Path Exists! Checking for already mapped partitions"
+                write-host "Path Exists!" -ForegroundColor Cyan
+                write-host " - Checking for already mapped partitions - " -NoNewline -ForegroundColor Yellow
                 #check for already mapped
-
+                write-host " Oh well Check Next time" -ForegroundColor Green
             }
             else
             {
-                Write-Host "Path does not exist. Creating"
+                Write-Host "Path does not exist." -ForegroundColor Green
+                write-host " - Creating Temp Path for WinRE - " -ForegroundColor Yellow -NoNewline
                 new-item -Path "c:\windows\temp\winre" -ItemType Directory
-                write-host "Created Path"
+                write-host "Created Path" -ForegroundColor Green
             }
 
-
+            write-host " - Mapping WinRE Partition to Temp Path - " -ForegroundColor Yellow -NoNewline
 
             Add-PartitionAccessPath -DiskNumber $winrePartition.DiskNumber -PartitionNumber $winrePartition.PartitionNumber -AccessPath c:\windows\temp\winre
 
+            write-host "Mapped WinRE to temp path" -ForegroundColor Green
             #Checking Partition for content
 
             #prompt to proceed
 
-            Write-host "Cehcking WinRE Partition:"
+            Write-host " - Checking WinRE Partition:" -NoNewline -ForegroundColor Yellow
             if (test-path -Path "C:\windows\temp\winre\Recovery\WindowsRE\Winre.wim")
             {
-                Write-Host "Found the WinRE WIM file and will proceed"
+                Write-Host "Found the WinRE WIM file and will proceed" -ForegroundColor Green
             }
             else
             {
-                write-host "Not sure on the WinRE partition so aborting"
+                write-host "Not sure on the WinRE partition so aborting" -ForegroundColor Red
+                write-host " - Cleaning Up" -ForegroundColor Red
+                write-host "   - Removing Mapping" -ForegroundColor Red
                 Remove-PartitionAccessPath -AccessPath "c:\windows\temp\winre" -DiskNumber $winrePartition.DiskNumber -PartitionNumber $winrePartition.PartitionNumber
+                Write-host "Ending Process" -ForegroundColor Red
                 Stop-Transcript
                 exit
             }
@@ -125,30 +133,50 @@ else
             Copy-Item -Path C:\windows\temp\winre\Recovery -Destination C:\Windows\Temp\winrenew -Container -Recurse
             write-host "Copy Finished"
             #remove Access Path
-            Write-host "Disconnecting maps" -NoNewline
-            Remove-PartitionAccessPath -AccessPath "C:\windows\temp\winreNew" -DiskNumber $newWinREPartition.DiskNumber -PartitionNumber $newWinREPartition.PartitionNumber
+            Write-host "Disconnecting mapped Access Paths" -NoNewline -ForegroundColor White
             
+            write-host " - Removing Mapping for New Recovery Partition - " -NoNewline -ForegroundColor Yellow
+            Remove-PartitionAccessPath -AccessPath "C:\windows\temp\winreNew" -DiskNumber $newWinREPartition.DiskNumber -PartitionNumber $newWinREPartition.PartitionNumber
+            write-host "Done" -ForegroundColor Green
+            
+            write-host " - Removing Mapping for Old Recovery Partition - " -NoNewline -ForegroundColor Yellow          
             Remove-PartitionAccessPath -AccessPath "c:\windows\temp\winre" -DiskNumber $winrePartition.DiskNumber -PartitionNumber $winrePartition.PartitionNumber
-
             Write-host " Done!"
+
+            Write-host "Fixing Recovery Partitions and Settings" -NoNewline -ForegroundColor White
+
+            write-host " - Diabling Recovery Partition - " -NoNewline -ForegroundColor Yellow
             C:\windows\system32\ReAgentc.exe /disable
+            write-host "Done" -ForegroundColor Green
+
+            write-host " - Removing old Recovery Partition - " -NoNewline -ForegroundColor Yellow
             Remove-Partition -DiskNumber $winrePartition.DiskNumber -PartitionNumber $winrePartition.PartitionNumber -Confirm:$false
+            write-host "Done" -ForegroundColor Green
+
+            write-host " - Setting Recovery Partition Path - " -NoNewline -ForegroundColor Yellow
             $winrepathset = "\\?\GLOBALROOT\device\harddisk$($newWinREPartition.DiskNumber)\partition$($newWinREPartition.PartitionNumber)\Recovery\WindowsRE"
             C:\Windows\System32\ReAgentc.exe /setreimage /path $winrepathset /target c:\Windows
+            write-host "Done" -ForegroundColor Green
+
+            write-host " - Enabling Recovery Partition - " -NoNewline -ForegroundColor Yellow
             C:\Windows\System32\ReAgentc.exe /enable
+            write-host "Done" -ForegroundColor Green
 
+            Write-host "Fixing Partitions" -NoNewline -ForegroundColor White
 
+            write-host " - Resizing Partition Before New Recovery Partition - " -NoNewline -ForegroundColor Yellow
             $size = (Get-PartitionSupportedSize -DiskNumber $winrePartition.DiskNumber -PartitionNumber ($winrePartition.PartitionNumber - 1))
-
             Resize-Partition -DiskNumber $winrePartition.DiskNumber -PartitionNumber ($winrePartition.PartitionNumber - 1) -Size $size.SizeMax
+            write-host "Done" -ForegroundColor Green
 
-
+            write-host " - Setting Attributes on Recovery Partition - " -NoNewline -ForegroundColor Yellow
             $null = @"
 select disk $($newWinREPartition.DiskNumber)
 select partition $($newWinREPartition.PartitionNumber)
 gpt attributes=0x8000000000000001
 exit
 "@ | diskpart.exe
+            write-host "Done" -ForegroundColor Green
         }
     }
 }
